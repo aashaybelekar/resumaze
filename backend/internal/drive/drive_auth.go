a0f869cfcbff6eec0822a50de3fd1f1ab0a6821f
aashaybelekar <aashaybelekar22@gmail.com>
2025-11-09 14:45:52 +0530

    feat: update token file path to enhance security and improve error handling in drive authentication

--- backend/internal/drive/drive_auth.go
+++ backend/internal/drive/drive_auth.go
@@ -2,81 +2,104 @@ package drive
 
 import (
 	"context"
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-    "golang.org/x/oauth2"
-    "golang.org/x/oauth2/google"
-    "google.golang.org/api/drive/v3"
-    "google.golang.org/api/option"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
 )
 
 func getClient(config *oauth2.Config) *http.Client {
-    tokenFile := "token.json"
+	tokenFile := "./secrets/token.json"
 
-    // Try loading saved token
-    tok, err := tokenFromFile(tokenFile)
-    if err != nil {
-        // Generate auth URL
-        authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-        fmt.Println("Go to the following link in your browser and paste the code here:")
-        fmt.Println(authURL)
+	// Try loading saved token
+	tok, err := tokenFromFile(tokenFile)
+	if err != nil {
+		// Generate auth URL
+		authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+		fmt.Println("Go to the following link in your browser and paste the code here:")
+		fmt.Println(authURL)
 
-        // Read code from console
-        fmt.Print("Enter authorization code: ")
-        var code string
-        fmt.Scan(&code)
+		// Read code from console
+		fmt.Print("Enter authorization code: ")
+		var code string
+		fmt.Scan(&code)
 
-        // Exchange code for token
-        tok, err = config.Exchange(context.Background(), code)
-        if err != nil {
-            log.Fatalf("Unable to retrieve token from web: %v", err)
-        }
+		// Exchange code for token
+		tok, err = config.Exchange(context.Background(), code)
+		if err != nil {
+			log.Fatalf("Unable to retrieve token from web: %v", err)
+		}
 
-        // Save token for future runs
-        saveToken(tokenFile, tok)
-    }
+		// Save token for future runs
+		saveToken(tokenFile, tok)
+	}
 
-    return config.Client(context.Background(), tok)
+	return config.Client(context.Background(), tok)
 }
-
 
 func tokenFromFile(file string) (*oauth2.Token, error) {
-    f, err := os.Open(file)
-    if err != nil {
-        return nil, err
-    }
-    defer f.Close()
-    tok := &oauth2.Token{}
-    err = json.NewDecoder(f).Decode(tok)
-    return tok, err
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
 }
 
-
 func saveToken(path string, token *oauth2.Token) {
-    f, err := os.Create(path)
-    if err != nil {
-        log.Fatalf("Unable to save OAuth token: %v", err)
-    }
-    defer f.Close()
-    json.NewEncoder(f).Encode(token)
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatalf("Unable to save OAuth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 func NewAuthDriveService(clientSecretPath string) (*drive.Service, error) {
-    b, err := os.ReadFile(clientSecretPath)
-    if err != nil {
-        return nil, err
-    }
+	fmt.Println("REACHED HERE!1")
+	b, err := os.ReadFile(clientSecretPath)
+	if err != nil {
+		return nil, err
+	}
 
-    config, err := google.ConfigFromJSON(b, drive.DriveScope)
-    if err != nil {
-        return nil, err
-    }
+	config, err := google.ConfigFromJSON(b, drive.DriveScope)
+	if err != nil {
+		fmt.Println("YEP THIS IS ISSUE.")
+		return nil, err
+	}
+	fmt.Println("REACHED HERE!c")
 
-    client := getClient(config)
-    return drive.NewService(context.Background(), option.WithHTTPClient(client))
+	client := getClient(config)
+	return drive.NewService(context.Background(), option.WithHTTPClient(client))
 }
 
+func NewAuthJWTDriveService(jsonPath string) (*drive.Service, error) {
+	ctx := context.Background()
+
+	b, err := os.ReadFile(jsonPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read service account file: %v", err)
+	}
+
+	config, err := google.JWTConfigFromJSON(b, drive.DriveFileScope)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse service account file: %v", err)
+	}
+
+	client := config.Client(ctx)
+
+	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create drive service: %v", err)
+	}
+
+	return srv, nil
+}
